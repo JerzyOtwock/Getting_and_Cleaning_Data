e9122deeb8689303259da5fb4fe9aef99e0da66d
unknown <jerzy.kwiatkowski@yahoo.pl>
2020-04-13 19:59:43 +0200

    hw_week_4

---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+library(dplyr)
+library(tidyverse)
+library(data.table)
+
+# read input files and rename columns
+
+featuresMap<-read.table("features.txt", header = FALSE)
+featuresMap$V2_unique <- make.names(featuresMap$V2,unique = TRUE)
+# featuresMap[featuresMap$V2 == "fBodyAcc-bandsEnergy()-1,8",] # label occurs many times
+activitiesMap<-read.table("activity_labels.txt", header = FALSE)
+colnames(activitiesMap) <- c("ActivityId" , "Activity")
+
+dfTrainX<-read.table("./train/X_train.txt", header = FALSE)
+dfTrainY<-read.table("./train/Y_train.txt", header=FALSE)
+dfTrainX2<-read.table("./train/subject_train.txt", header=FALSE)
+dfTestX<-read.table("./test/X_test.txt", header = FALSE)
+dfTestY<-read.table("./test/Y_test.txt", header=FALSE)
+dfTestX2<-read.table("./test/subject_test.txt", header=FALSE)
+
+colnames(dfTrainX)<-featuresMap$V2_unique
+colnames(dfTestX)<-featuresMap$V2_unique
+colnames(dfTrainY)<-"ActivityId"
+colnames(dfTestY)<-"ActivityId"
+colnames(dfTrainX2)<-"Subject"
+colnames(dfTestX2)<-"Subject"
+
+#### question 1
+
+df1 <- cbind(dfTrainX, dfTrainY, dfTrainX2 )
+df2 <- cbind(dfTestX, dfTestY,  dfTestX2)
+df <- rbind(df1, df2)
+
+#### question 2
+
+df_filterd <- df %>% 
+    select(contains("mean"), contains("std"), contains("Subject"), contains("Activity"))
+
+#### question 3
+
+df_rename <- df_filterd
+df_rename <- merge(df_rename , activitiesMap, by = "ActivityId" )
+
+#### question 5
+
+df_5 <- df_rename %>% arrange(Subject , Activity)
+df_5 <- data.table(df_5)
+df_6 <-  aggregate(x = df_5 ,               
+          by = list(df_5$Subject   , df_5$Activity),           
+          FUN = mean)                          
+
+write.table(df_6, 'output.txt', row.names = F)
